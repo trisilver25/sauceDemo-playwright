30b1da117176bcb6657e06ff733c4b9d7e6eb34c
Tristin Smith <85770698+trisilver25@users.noreply.github.com>
2026-06-29 19:26:37 -0400

    Add-to-cart spec now adds the 1st product item on the inventory page to the cart, then confirms when in the cart that the product names match.

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -1,11 +1,33 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("Add a product to cart", async ({ page }) => {
   // Go to products page
   await page.goto("/inventory.html");
 
-  // Get the "Add to Cart" button assign to a variable
-  let addButton = page.getByRole("button", {
-    name: "Add to Cart",
+  // Retrieve the first product item
+  let firstProductCard = page.locator(".inventory_item").first();
+
+  // Store the products name from the 1st product card.
+  let firstProductName = await firstProductCard
+    .locator(".inventory_item_name")
+    .textContent();
+
+  // Store the add to cart button from the 1st product card.
+  let firstButton = await firstProductCard.getByRole("button", {
+    name: "Add to cart",
   });
+
+  // Click "Add to Cart"
+  await firstButton.click();
+
+  // Click "Shopping Cart"
+  await page.locator(".shopping_cart_link").click();
+
+  // Pull the recently added cart item's name.
+  let cartProductName = await page
+    .locator(".inventory_item_name")
+    .textContent();
+
+  // Confirm the 1st product name matches the cart name
+  expect(firstProductName).toEqual(cartProductName);
 });
